3ae5c2324bd7aa7a6b607105b38572667c04fbe0
yshagit31 <yshashank024@gmail.com>
2025-06-13 18:48:13 +0530

    Hero align fix 7

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -98,8 +98,8 @@ const Hero = () => {
   return (
     <section className='min-h-screen w-full flex flex-col relative' id="home">
       <Sparkles/>
-      <div className='w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3'>
-        <p className='sm:text-3xl text-2xl font-medium text-white text-center font-generalsans'> Hi, I am Shashank 
+      <div className='w-full mx-auto flex flex-col sm:mt-28 mt-20 c-space gap-3'>
+        <p className='sm:text-3xl text-2xl font-medium text-white text-center font-generalsans '> Hi, I am Shashank 
           <span className='waving-hand'> 🖐</span> </p>
     
             <h1 className="hero_tag text-gray_gradient flex justify-center items-center gap-2 flex-wrap">
